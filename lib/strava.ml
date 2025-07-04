8ecd8d0da77552a591002ffc0c772c55ab149ff9
Angel ILIEV <a.v.iliev13@gmail.com>
2025-07-04 12:51:18 +0300

    wip: activity parsing

--- lib/strava.ml
+++ lib/strava.ml
@@ -43,7 +43,7 @@ type streamType =
   | IntOptStream of int option Stream.t
   | FloatStream of float Stream.t
   | TupleStream of float list Stream.t
-[@@deriving show, yojson_of]
+[@@deriving show { with_path = false }, yojson_of]
 
 let streamType_of_yojson (json : Yojson.Safe.t) : streamType =
   match Yojson.Safe.Util.member "data" json with
@@ -60,7 +60,7 @@ let streamType_of_yojson (json : Yojson.Safe.t) : streamType =
   | _ -> failwith "unsupported"
 
 module Streams = struct
-  type t = streamType list [@@deriving show, yojson]
+  type t = streamType list [@@deriving show { with_path = false }, yojson]
 
   let example () =
     [ IntStream (Stream.int_example ()); FloatStream (Stream.float_example ()) ]
@@ -124,46 +124,39 @@ type sportType =
   | Windsurf
   | Workout
   | Yoga
-[@@deriving yojson]
+[@@deriving yojson, show { with_path = false }]
 
 module StravaPolylineMap = struct
-  type t = { id : string; summary_polyline : string } [@@deriving yojson]
+  type t = { id : string; summary_polyline : string }
+  [@@deriving yojson, show { with_path = false }] [@@yojson.allow_extra_fields]
 end
 
-(* TODO: do i need to take their activity summary or i can just calculate it myself based on the raw data? *)
-(* this might be prefferable cause their activity object might be
-   hard to parse because some fields are only present under
-   certain conditions *)
+module StravaAthlete = struct
+  type t = { id : int }
+  [@@deriving yojson, show { with_path = false }] [@@yojson.allow_extra_fields]
+end
+
 module StravaActivity = struct
   type t = {
+    athlete : StravaAthlete.t;
     name : string;
-    distance : float;
-    moving_time : int;
-    elapsed_time : int;
-    total_elevation_gain : float;
-    sport_type : sportType;
+    (* TODO: convert to correct sport type and not just string *)
+    (* sport_type : sportType; *)
+    sport_type : string;
     id : int;
     start_date_local : string;
     timezone : string;
     map : StravaPolylineMap.t;
-    start_latlng : float * float;
-    end_latlng : float * float;
-    average_speed : float;
-    max_speed : float;
-    average_cadence : float;
-    average_temp : int;
-    has_heartrate : bool;
-    (* TODO: the following 2 are not pressend if has_heartrate is false *)
-    (* handle that*)
-    average_heartrate : float;
-    max_heartrate : float;
+    gear_id : string;
   }
-  [@@deriving yojson]
+  [@@deriving yojson, show { with_path = false }] [@@yojson.allow_extra_fields]
 end
 
 module StravaActivities = struct
-  type t = StravaActivity.t list [@@deriving yojson]
+  type t = StravaActivity.t list [@@deriving yojson, show { with_path = false }]
 end
+
+(* TODO: Create a custom activity class that has all the properties calculated from the streams *)
 
 let list_activities token n =
   let url = Uri.of_string "https://www.strava.com/api/v3/athlete/activities" in
@@ -241,19 +234,19 @@ let%expect_test "deserialize get_stream.json" =
   printf "%s" (Streams.show streams);
   [%expect
     {|
-    [(Strava.IntStream
+    [(IntStream
         { type_ = "time";
           data =
           [0; 1; 2; 3; 4; 5; 6; 7; 8; 9; 10; 11; 12; 13; 14; 15; 16; 17; 18; 19;
             20];
           series_type = "distance"; original_size = 21; resolution = "high" });
-      (Strava.FloatStream
+      (FloatStream
          { type_ = "distance";
            data =
            [0.; 2.8; 5.5; 8.3; 11.; 13.8; 16.5; 19.3; 22.; 27.; 32.; 36.; 39.;
              42.; 45.; 48.; 51.; 54.; 57.; 60.; 64.];
            series_type = "distance"; original_size = 21; resolution = "high" });
-      (Strava.TupleStream
+      (TupleStream
          { type_ = "latlng";
            data =
            [[54.70304; 25.317412]; [54.703055; 25.317374];
@@ -267,32 +260,32 @@ let%expect_test "deserialize get_stream.json" =
              [54.703245; 25.316665]; [54.703255; 25.31662];
              [54.703265; 25.316572]; [54.703273; 25.316523]];
            series_type = "distance"; original_size = 21; resolution = "high" });
-      (Strava.FloatStream
+      (FloatStream
          { type_ = "altitude";
            data =
            [115.2; 115.2; 115.2; 115.2; 115.2; 115.2; 115.2; 115.2; 115.2; 114.6;
              114.2; 114.; 113.8; 113.8; 113.4; 113.4; 113.2; 113.2; 112.8; 112.8;
              112.6];
            series_type = "distance"; original_size = 21; resolution = "high" });
-      (Strava.FloatStream
+      (FloatStream
          { type_ = "velocity_smooth";
            data =
            [0.; 0.; 2.75; 2.75; 2.75; 2.75; 2.75; 2.75; 2.75; 3.2; 3.65; 3.9;
              3.95; 4.; 3.6; 3.2; 3.; 3.; 3.; 3.; 3.2];
            series_type = "distance"; original_size = 21; resolution = "high" });
-      (Strava.IntStream
+      (IntStream
          { type_ = "heartrate";
            data =
            [80; 79; 78; 77; 77; 75; 75; 75; 76; 78; 80; 80; 82; 84; 87; 89; 91;
              95; 97; 99; 100];
            series_type = "distance"; original_size = 21; resolution = "high" });
-      (Strava.IntStream
+      (IntStream
          { type_ = "cadence";
            data =
            [84; 84; 84; 84; 84; 84; 84; 84; 84; 85; 86; 86; 86; 85; 84; 83; 82;
              82; 82; 82; 82];
            series_type = "distance"; original_size = 21; resolution = "high" });
-      (Strava.IntOptStream
+      (IntOptStream
          { type_ = "watts";
            data =
            [None; None; None; None; None; None; None; None; (Some 283);
@@ -300,13 +293,13 @@ let%expect_test "deserialize get_stream.json" =
              (Some 219); (Some 220); (Some 201); (Some 209); (Some 193);
              (Some 200); (Some 198)];
            series_type = "distance"; original_size = 21; resolution = "high" });
-      (Strava.IntStream
+      (IntStream
          { type_ = "temp";
            data =
            [29; 29; 29; 29; 29; 29; 29; 29; 29; 29; 29; 29; 29; 29; 29; 29; 29;
              29; 29; 29; 29];
            series_type = "distance"; original_size = 21; resolution = "high" });
-      (Strava.FloatStream
+      (FloatStream
          { type_ = "grade_smooth";
            data =
            [0.; 0.; 0.; 0.; 0.; 0.; 0.; -4.5; -6.5; -7.2; -8.2; -5.3; -6.2; -5.;
@@ -314,3 +307,22 @@ let%expect_test "deserialize get_stream.json" =
            series_type = "distance"; original_size = 21; resolution = "high" })
       ]
     |}]
+
+let%expect_test "deserialize activity" =
+  let json =
+    Yojson.Safe.from_file "/home/angel/Documents/ocaml/unto/one_activity.json"
+  in
+  let activities = StravaActivities.t_of_yojson json in
+  printf "%s" (StravaActivities.show activities);
+  [%expect
+    {|
+    [{ athlete = { id = 3504239 }; name = "Afternoon Run"; sport_type = "Run";
+       id = 14995177737; start_date_local = "2025-07-03T16:29:12Z";
+       timezone = "(GMT+02:00) Europe/Vilnius";
+       map =
+       { id = "a14995177737";
+         summary_polyline =
+         "wmemIkqzyCiAtDIv@k@bC}@jC_AxFWt@Pm@lA}GpBiGlAuEHq@Tk@t@eHD}@]oHQaA_@}@WwBOkBGIMw@aBlBu@^cAbAg@|@_AhAo@dBu@bAe@hAg@t@oAhAqDXc@a@g@]q@QCNJJXEnAjADv@f@~@FfDpBbHv@|Af@`Db@VHz@Nd@ZVKgAb@}GCaDL}@D{BRkAR[\\}APER_@b@}AVcBlAmCb@o@ZQhAJGO_@KS_@WmAsAmEo@oAGk@Wu@o@Gw@RKZBl@QcAWC]k@kBuBo@oBEcAJu@GMe@[YJoAcAe@_ACOFo@XuAAo@FoAHC?SLm@Ay@I_@a@u@_@KKLz@jAH^KrAGbB`@mBDo@Me@?]ZcBj@aAPGDPRJFIq@~Ag@zA[jB[r@Kl@c@Da@d@JPFAI@Rh@?RMu@@QKUd@OV{@Bo@GYIE\\Sh@sB\\Ed@k@Jq@R_@t@D^Vz@i@nA@T_ATUl@qAHgAf@OFR~@\\FF@ZRc@fAy@P?TRbAELr@VGNZ^JeAz@g@MWv@TQrAPNW|@uCp@wCPK`B`BvAbBt@f@Zk@`AqD\\aCpBkGBwAJSVGf@TbAo@n@aCj@n@|@d@l@TZCj@^nBWjAuALw@NBPa@XIH_@\\a@VWZOTy@VoAXyBt@aCh@aAJc@T_Ch@yCZo@Nu@Ng@VE|@zAUiCGU]WmA\\eCEQJe@fABc@E{@_AgDs@e@QmAEaAw@kCEo@y@m@c@q@gBIo@g@o@Ob@}A|@k@Za@l@G~@o@dCCDd@MfBBrAE|@m@lAu@bAc@lCBdBGp@@~@b@rAl@`@i@Sk@_AmARw@a@eAmAaAUWg@?Z[hAAn@Hf@Mb@?XVr@n@p@n@pAIt@Bb@GpB[t@SjAMPAj@S~@EzAIFAh@DrA_@|@Mx@cAfAI\\WRMVO~Ak@nBS`@BdAEVU\\Op@cA^Ya@]^E[o@FELFRVPVv@h@ZNf@YnBqAnEUh@K~@dBzC\\z@PwACYQACMLQ@_@a@RQj@UF_A_BKAKVeA|F_@pAYVm@LG\\]n@Qr@JhAEf@jBnBXl@@pEDJ|@j@LZJfAuAHw@bAoBz@M?[c@YMeCZTlCXpBs@{E}@eMk@}C^rBx@xGX~E"
+         };
+       gear_id = "g272465" }
+      ] |}]
