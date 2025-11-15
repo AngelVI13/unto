7c6a0e9b409e38772bfda3aefbb0f77f1e0c82c9
Angel ILIEV <a.v.iliev13@gmail.com>
2025-11-15 20:37:54 +0200

    add users table queries

--- lib/db/generate_dbops.sh
+++ lib/db/generate_dbops.sh
@@ -1,2 +1,3 @@
 #!/usr/bin/bash
 sqlgg -gen caml -name DbOps -params named ./queries.sql > db_ops.ml
+sqlgg -gen caml -name UsersOps -params named ./users.sql > users_ops.ml
---
+++ lib/db/users.sql
@@ -0,0 +1,18 @@
+-- @create_users
+CREATE TABLE IF NOT EXISTS users (
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
+    user TEXT NOT NULL,
+    db_name TEXT NOT NULL,
+    hostname TEXT NOT NULL,
+    token TEXT NOT NULL
+);
+
+-- @add_user
+INSERT INTO users VALUES;
+
+-- @user_by_name
+SELECT * FROM users u WHERE u.user = @user_name;
+
+-- @remove_user
+DELETE FROM users WHERE user = @user_name;
+
